280aab5d313e90b11d6d2926e64839bb1f378ce6
vinod <infovinodjangid@gmail.com>
2023-03-23 12:42:30 +0530

    API Key Update

--- script.js
+++ script.js
@@ -4,37 +4,37 @@ window.addEventListener('load',()=>{
     preloader.style.display = "none";
 });
 
-// location script
-const button = document.getElementById('button');
-const Location = document.getElementById('location')
+// LocationHeading script
+const button = document.getElementById("button");
+const Heading = document.getElementById("locationHeading");
 button.addEventListener("click", ()=>{
-    if(navigator.geoLocation){
-        Location.innerText = "Allow to detect Location";
-        navigator.geoLocation.getCurrentPosition(onSuccess, onError);
+    if(navigator.geolocation){
+        Heading.innerText = "Allow to detect location";
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }else{
-        Location.innerText = "Your browser not support :(";
+        Heading.innerText = "Your browser not support";
     }
 });
 function onSuccess(position){
-    Location.innerText = "Detecting your Location...";
+    Heading.innerText = "Detecting your location...";
     let {latitude, longitude} = position.coords;
-    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=YOUR_API_KEY`)
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=5e70e8753b3546f39c7d4b82e13989ec`)
     .then(response => response.json()).then(response =>{
         let allDetails = response.results[0].components;
         console.table(allDetails);
         let {county, postcode, country} = allDetails;
-        Location.innerText = `${county} ${postcode}, ${country}`;
+        Heading.innerText = `${county} ${postcode}, ${country}`;
     }).catch(()=>{
-        Location.innerText = "Something went wrong";
+        Heading.innerText = "Something went wrong";
     });
 }
 function onError(error){
     if(error.code == 1){
-        Location.innerText = "You denied the request";
+        Heading.innerText = "You denied the request";
     }else if(error.code == 2){
-        Location.innerText = "Location is unavailable";
+        Heading.innerText = "Location is unavailable";
     }else{
-        Location.innerText = "Something went wrong";
+        Heading.innerText = "Something went wrong";
     }
-    button.setAttribute("disabled", "true");
+    // button.setAttribute("disabled", "true");
 }
